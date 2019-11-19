32460011438dcfef1efbb0b756c241cf21a35ab9
Bernd Kuhlen <bernd.kuhlen@wetteronline.de>
2019-11-19 09:42:47 +0100

    BK

--- python/numbers.py
+++ python/numbers.py
@@ -1,11 +1,11 @@
 # Main Solution
-for num in range(1,21):
-	if num == 4 or num == 13:
-		print(f"{num} is unlucky")
-	elif num % 2 == 0:
-		print(f"{num} is even")
-	else:
-		print(f"{num} is odd")
+#for num in range(1,21):
+#	if num == 4 or num == 13:
+#		print(f"{num} is unlucky")
+#	elif num % 2 == 0:
+#		print(f"{num} is even")
+#	else:
+#		print(f"{num} is odd")
 
 	
 # Slight Refactor
