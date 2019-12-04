627f209ed0961c7ec85156df439bf06accad7dae
Bernd Kuhlen <bernd.kuhlen@wetteronline.de>
2019-12-04 18:21:20 +0100

    BK

--- python/boto3/waiter_for_ec2.py
+++ python/boto3/waiter_for_ec2.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import boto3
 import time
-session=boto3.session.Session(profile_name="dev_root")
-ec2_re=session.resource(service_name="ec2",region_name="us-east-1")
-my_in=ec2_re.Instance(id="i-04d79d94e0415e891")
+session = boto3.session.Session(profile_name="sandbox")
+ec2_re = session.resource(service_name="ec2", region_name="us-east-1")
+id = input("instance id, please ")
+my_in = ec2_re.Instance(id=id)
 my_in.start()
 my_in.wait_until_running()
