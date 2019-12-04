4002821fbe76f335f66af3cc248b13fe7aa2193d
Bernd Kuhlen <bernd.kuhlen@wetteronline.de>
2019-12-04 20:14:30 +0100

    BK

---
+++ python/boto3/get_running_instances.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import boto3
+
+session = boto3.session.Session(profile_name="sandbox")
+
+ec2_con_re = session.resource(service_name="ec2", region_name="us-east-1")
+
+f1 = {"Name": "availability-zone", "Values": ['us-east-1b']}
+f2 = {"Name": "instance-state-name", "Values": ['running']}
+for each_in in ec2_con_re.instances.filter(Filters=[f1, f2]):
+    print(each_in.id, each_in.state)
