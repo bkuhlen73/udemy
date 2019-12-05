e61bb22cf18c0acd111f8359548f61a39447e890
Bernd Kuhlen <bernd.kuhlen@wetteronline.de>
2019-12-05 18:11:39 +0100

    BK

--- python/boto3/get_all_regions_for_ec2_service.py
+++ python/boto3/get_all_regions_for_ec2_service.py
@@ -1,11 +1,15 @@
-#!/bin/python
+#!/usr/bin/env python3
 import boto3
-session=boto3.session.Session(profile_name="dev_root")
-ec2_cli=session.client(service_name="ec2",region_name="us-east-1")
+session = boto3.session.Session(profile_name="sandbox")
+ec2_cli = session.client(service_name="ec2", region_name="us-east-1")
 
-regions=[]
-for each_region_info in  ec2_cli.describe_regions().get('Regions'):
-  regions.append( each_region_info.get('RegionName'))
+regions = []
+# for each_region_info in ec2_cli.describe_regions().get('Regions'):
+#     regions.append(each_region_info.get('RegionName'))
 
+print(ec2_cli.describe_regions().get('Regions'))
 
-print "All regions for ec2 service are: \n",regions
+# for each_region_info in ec2_cli.describe_regions().get('Regions'):
+#     regions.append(each_region_info.get('RegionName'))
+
+print(f"All regions for ec2 service are: {regions}\n")
--- python/boto3/get_aws_account_id.py
+++ python/boto3/get_aws_account_id.py
@@ -1,7 +1,8 @@
-#!/bin/python
+#!/usr/bin/env python3
 import boto3
 
-session=boto3.session.Session(profile_name="dev_root")
-sts_client=session.client("sts")
+session = boto3.session.Session(profile_name="sandbox")
+sts_client = session.client("sts")
 
-print sts_client.get_caller_identity().get('Account')
+print(sts_client.get_caller_identity().get('Account'))
+print(sts_client.get_caller_identity())
