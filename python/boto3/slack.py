fe3c4a7ef2d5922625d0f2a3e1d89360b9684cd5
bkuhlen73 <40298628+bkuhlen73@users.noreply.github.com>
2019-12-10 16:06:57 +0100

    Delete slack.py

--- python/boto3/slack.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-import json
-
-web_hook_url = 'https://hooks.slack.com/services/TE5A4J5EW/BR36BSJJV/CJrbI6cPMqxxxxxxxxxxxxxxxx'
-slack_msg = {'text': 'Python-Slack-Test von Bernd'}
-
-requests.post(web_hook_url, data=json.dumps(slack_msg))
