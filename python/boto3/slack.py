3196e04ac96c6e04208e4bd6f0180e77e06a5897
Bernd Kuhlen <bernd.kuhlen@wetteronline.de>
2019-12-10 11:50:42 +0100

    BK:

--- python/boto3/slack.py
+++ python/boto3/slack.py
@@ -3,7 +3,7 @@
 import requests
 import json
 
-web_hook_url = 'https://hooks.slack.com/services/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'
+web_hook_url = 'https://hooks.slack.com/services/TE5A4J5EW/BR36BSJJV/CJrbI6cPMqxxxxxxxxxxxxxxxx'
 slack_msg = {'text': 'Python-Slack-Test von Bernd'}
 
 requests.post(web_hook_url, data=json.dumps(slack_msg))
